35b447bb7bc5df7282429518eb167440bac53646
Jul <oknormyasn@gmail.com>
2017-02-27 15:19:04 +0300

    Employee

--- DZ_Pogorelova/Controllers/EmployeeController.cs
+++ DZ_Pogorelova/Controllers/EmployeeController.cs
@@ -9,15 +9,24 @@ namespace DZ_Pogorelova.Controllers
 {
     public class EmployeeController : Controller
     {
-        // GET: Employee
-        public ActionResult Index()
-        {
-            List<Employee> empl = new List<Employee>
+        List<Employee> empl = new List<Employee>
                     {
                         new Employee(){ Id=1, FirstName = "Jack", Age = 8, LastName = "Smith",MiddleName = "Ivanovich",PersonCard="Horooch",Position = "Developer" },
                         new Employee(){ Id=2, FirstName = "Ваня", Age = 18, LastName = "Боярышкин",MiddleName = "Камекгбекович",PersonCard="Horooch",Position = "Disigner" },
                         new Employee(){ Id=3, FirstName = "Василий", Age = 81, LastName = "Трошкин",MiddleName = "Петрович",PersonCard="Horooch",Position = "Analytic" } };
+        // GET: Employee
+        public ActionResult Index()
+        {
+            
             return View(empl);
+        }
+
+
+
+        public ActionResult Details(int id)
+        {
+           // Employee emp = empl.Where(x => x.Id == id).FirstOrDefault();
+            return View(empl.Where(x => x.Id == id).FirstOrDefault());
         }
     }
 }
