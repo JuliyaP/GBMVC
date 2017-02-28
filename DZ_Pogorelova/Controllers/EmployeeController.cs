a8d410cfbcf545b3fc2b9ff7e0fa207d3594383d
Jul <oknormyasn@gmail.com>
2017-02-28 16:56:41 +0300

    stat class with employee

--- DZ_Pogorelova/Controllers/EmployeeController.cs
+++ DZ_Pogorelova/Controllers/EmployeeController.cs
@@ -3,30 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using DZ_Pogorelova.Models;
+using DZ_Pogorelova.Utils;
 
 namespace DZ_Pogorelova.Controllers
 {
     public class EmployeeController : Controller
     {
-        List<Employee> empl = new List<Employee>
-                    {
-                        new Employee(){ Id=1, FirstName = "Jack", Age = 8, LastName = "Smith",MiddleName = "Ivanovich",PersonCard="Horooch",Position = "Developer" },
-                        new Employee(){ Id=2, FirstName = "Ваня", Age = 18, LastName = "Боярышкин",MiddleName = "Камекгбекович",PersonCard="Horooch",Position = "Disigner" },
-                        new Employee(){ Id=3, FirstName = "Василий", Age = 81, LastName = "Трошкин",MiddleName = "Петрович",PersonCard="Horooch",Position = "Analytic" } };
-        // GET: Employee
+        
+  
         public ActionResult Index()
         {
             
-            return View(empl);
+            return View(Emloyee.empl);
         }
 
 
 
         public ActionResult Details(int id)
         {
-           // Employee emp = empl.Where(x => x.Id == id).FirstOrDefault();
-            return View(empl.Where(x => x.Id == id).FirstOrDefault());
+            return View(Emloyee.empl.Where(x => x.Id == id).FirstOrDefault());
         }
     }
 }
---
+++ DZ_Pogorelova/Utils/Emloyee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DZ_Pogorelova.Models;
+
+namespace DZ_Pogorelova.Utils
+{
+    public static class Emloyee
+    {
+      public static   List<Employee> empl = new List<Employee>
+                    {
+                        new Employee(){ Id=1, FirstName = "Анна", Age = 8, LastName = "Тимофеева",MiddleName = "Александровна",PersonCard=" Умение работать в коллективе. Целеустремленность, аналитический склад ума. Плюс ответственность и бла-бла-бла",Position = "Developer" },
+                        new Employee(){ Id=2, FirstName = "Иван", Age = 18, LastName = "Мурзиков",MiddleName = "Григорьевич",PersonCard="Horooch",Position = "Disigner" },
+                        new Employee(){ Id=3, FirstName = "Василий", Age = 81, LastName = "Трошкин",MiddleName = "Петрович",PersonCard="Horooch",Position = "Analytic" } };
+    }
+}
